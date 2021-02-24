f3767f455803b31bfaf0d9ae9892dc2ba7e007d7
Herminia Woodwright <hwoodwri@si-a5.kzn.21-school.ru>
2021-02-24 23:40:56 +0300

    metallb

--- srcs/setup.sh
+++ srcs/setup.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
 
-# installing minikube
-# Minikube is a tool that allows you to easily run Kubernetes on a local machine
 #brew install minikube
 #brew reinstall minikube
 
-# installing kubectl
-# the kubectl command line tool, that lets you control Kubernetes clusters
 #brew install kubectl
 #brew reinstall kubectl
 
 
-
-
+#-------------------------------#
 
 # start a cluster
 minikube start --vm-driver=virtualbox
@@ -23,4 +18,8 @@ minikube status
 #minikube dashboard
 
 eval $(minikube docker-env)
+
+minikube addons enable matallb
+
+
 
