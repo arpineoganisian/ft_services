f3225617319df2655a2e4486cba6369779352d16
Herminia Woodwright <hwoodwri@si-f5.kzn.21-school.ru>
2021-03-16 12:12:21 +0300

    services

--- k8s_init.sh
+++ k8s_init.sh
@@ -5,8 +5,11 @@
 brew install minikube
 brew install kubectl
 
+minikube start --vm-driver=virtualbox
+
 mv /Users/hwoodwri/.brew/Cellar/minikube /Users/hwoodwri/goinfre/minikube
 ln -s /Users/hwoodwri/goinfre/minikube /Users/hwoodwri/.brew/Cellar/minikube
 
 mv /Users/hwoodwri/.minikube /Users/hwoodwri/goinfre/.minikube
 ln -s /Users/hwoodwri/goinfre/.minikube /Users/hwoodwri/.minikube
+
--- restart_minikube.sh
+++ restart_minikube.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-#minikube stop && minikube delete
+minikube stop && minikube delete
 minikube start --vm-driver=virtualbox
 minikube addons enable metallb
-kubectl delete svc nginx-svc
-kubectl delete deploy nginx-deploy
+kubectl delete svc nginx-svc && kubectl delete deploy nginx-deploy
 eval $(minikube docker-env)
 docker build -t nginx-image .
 kubectl apply -f ../configmap.yaml
