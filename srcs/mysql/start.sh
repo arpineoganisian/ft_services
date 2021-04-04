2a8f8645b4ba5ec49ff9d066fd1a73b2bff4f9c8
Herminia Woodwright <hwoodwri@si-d3.kzn.21-school.ru>
2021-04-04 16:37:44 +0300

    pma mysql wp

--- srcs/mysql/start.sh
+++ srcs/mysql/start.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-db_name='my_database'
+db_name='wordpress'
 username='root'
-hostname='localhost'
+hostname='%'
+# значение '%' в поле Host означает любое имя хоста
 
 rc default
 /etc/init.d/mariadb setup
@@ -10,10 +11,10 @@ rc-service mariadb start
 
 mysql -e "CREATE DATABASE $db_name;"
 mysql -e "GRANT ALL PRIVILEGES ON $db_name.* TO '$username'@'$hostname' WITH GRANT OPTION;"
-mysql -e "UPDATE mysql.user SET plugin='mysql_native_password' WHERE user='$username';"
+#mysql -e "UPDATE mysql.user SET plugin='mysql_native_password' WHERE user='$username';"
 mysql -e "FLUSH PRIVILEGES;"
 
-mysql -uroot my_database < my_database.sql
+mysql -uroot wordpress < wordpress.sql
 rc-service mariadb stop
 exec mysqld_safe
 
