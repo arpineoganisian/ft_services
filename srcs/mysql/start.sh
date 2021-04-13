950bc16dae9d0704ff0307d17fdc94307c53f0d6
Herminia Woodwright <hwoodwri@si-d3.kzn.21-school.ru>
2021-04-13 21:44:43 +0300

    telegraf

--- srcs/mysql/start.sh
+++ srcs/mysql/start.sh
@@ -7,21 +7,14 @@ rc default
 /etc/init.d/mariadb setup
 rc-service mariadb start
 
-# создание бд
+# создание базы данных
 mysql -e "CREATE DATABASE $db_name;"
-
-# создание нового пользователя с помощью команды GRANT
-# eсли Вы даете привилегии пользователю, который не существует, он будет автоматически создан
+# команда GRANT - eсли Вы даете привилегии пользователю, который не существует, он будет автоматически создан
 mysql -e "GRANT ALL PRIVILEGES ON $db_name.* TO '$username'@'%' IDENTIFIED BY 'arpine' WITH GRANT OPTION;"
 # значение '%' в поле Host означает любое имя хоста
-
-#mysql -e "UPDATE mysql.user SET plugin='mysql_native_password' WHERE user='$username';"
-
 mysql -e "FLUSH PRIVILEGES;"
 
-# запись бд с юзерами в только что созданную бд 
+# запись б.д. с юзерами в только что созданную бд 
 mysql -uroot wordpress < wordpress.sql
 rc-service mariadb stop
-exec mysqld_safe
-
-# --execute (or -e) option  can be used with mysql to pass one or more semicolon-separated SQL statements to the server
+# exec mysqld_safe
