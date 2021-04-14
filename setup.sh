1c7a57e19ba42ce8b42be5f5a71dba62f5da2ff3
Herminia Woodwright <hwoodwri@si-d3.kzn.21-school.ru>
2021-04-14 22:43:39 +0300

    cleanup

--- setup.sh
+++ setup.sh
@@ -5,8 +5,9 @@ minikube start --vm-driver=virtualbox
 minikube addons enable metallb
 eval $(minikube docker-env)
 
-docker pull metallb/speaker:v0.8.2
-docker pull metallb/controller:v0.8.2
+# если metallb не выдает IP
+# docker pull metallb/speaker:v0.8.2
+# docker pull metallb/controller:v0.8.2
 
 #METALLB
 kubectl apply -f srcs/configmap.yaml
