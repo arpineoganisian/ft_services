5cc259b49ffc1dce334e88badf1cd5f5a320d4dc
Herminia Woodwright <hwoodwri@si-d3.kzn.21-school.ru>
2021-04-15 16:26:27 +0300

    copy

--- restart_influxdb.sh
+++ restart_influxdb.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 kubectl delete svc influxdb-svc && kubectl delete deploy influxdb-deploy 
+kubectl delete pv influxdb-pv
 eval $(minikube docker-env)
 docker build -t influxdb-image srcs/influxdb/
 kubectl apply -f srcs/influxdb/influxdb.yaml
--- restart_mysql.sh
+++ restart_mysql.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 kubectl delete svc mysql-svc && kubectl delete deploy mysql-deploy
+kubectl delete pv mysql-pv
 eval $(minikube docker-env)
 docker build -t mysql-image srcs/mysql/
 kubectl apply -f srcs/mysql/mysql.yaml
