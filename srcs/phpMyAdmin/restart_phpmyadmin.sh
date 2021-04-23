29246650c7e2e4739988ae9d7fdf9eecc39b2827
Herminia Woodwright <hwoodwri@si-n4.kzn.21-school.ru>
2021-04-23 20:51:59 +0300

    pma

--- srcs/phpMyAdmin/restart_phpmyadmin.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-kubectl delete svc phpmyadmin-svc && kubectl delete deploy phpmyadmin-deploy
-eval $(minikube docker-env)
-docker build -t phpmyadmin-image srcs/phpmyadmin/
-kubectl apply -f srcs/phpmyadmin/phpmyadmin.yaml
